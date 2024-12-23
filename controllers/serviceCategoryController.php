9c35cad3719411fa5958a1da2cdf195f90c14168
Nafeesa Yousaf <nafeesayousaf2129@gmail.com>
2024-12-23 20:28:48 +0500

    fetch all services

--- controllers/serviceCategoryController.php
+++ controllers/serviceCategoryController.php
@@ -3,10 +3,32 @@
 include('C:/xampp/htdocs/Service Link/includes/db.php'); 
 include('C:/xampp/htdocs/Service Link/models/ServiceCategoryModel.php');
 
-function getServiceCategories() {
+function getTopServiceCategories() {
     global $conn;
     
     $query = "SELECT * FROM serviceCategory LIMIT 4";  // Adjust table name for proper spelling
+    
+    $result = $conn->query($query);
+    
+    $serviceCategories = [];
+    
+    while ($row = $result->fetch_assoc()) {
+        $serviceCategory = new ServiceCategoryModel(
+            $row['category_id'], 
+            $row['category_name'], 
+            $row['category_image']
+        );
+        
+        $serviceCategories[] = $serviceCategory;
+    }
+    
+    return $serviceCategories;
+}
+
+function getAllServiceCategories() {
+    global $conn;
+    
+    $query = "SELECT * FROM serviceCategory";  // Adjust table name for proper spelling
     
     $result = $conn->query($query);
     
