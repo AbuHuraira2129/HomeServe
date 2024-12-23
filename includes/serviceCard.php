fad9eab01cb400069b7752f489c6fa206a0500ee
Nafeesa Yousaf <nafeesayousaf2129@gmail.com>
2024-12-23 18:06:07 +0500

    services component

---
+++ includes/serviceCard.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../assets/css/serviceCard.css">
+
+<div class="service-card">
+    <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
+    <h3><?php echo $title; ?></h3>
+    <button onclick="location.href='<?php echo $link; ?>'">Explore</button>
+</div>
+
