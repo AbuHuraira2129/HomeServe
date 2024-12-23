a5cba4cc880cf6feb9a73bca78fe665cc3f5b577
Nafeesa Yousaf <nafeesayousaf2129@gmail.com>
2024-12-23 18:08:31 +0500

    service model

---
+++ models/service.php
@@ -0,0 +1,14 @@
+<?php
+
+class Service {
+
+    public $id;
+    public $category;
+    public $sub_category;
+    public $description;
+    public $price_range;
+    public $availability;
+    public $image;
+}
+
+?>
